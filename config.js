f1d35cfd6ffb00626be70d9ceec1db483bfd1f9e
Vercyx <xxsketchmcxx@gmail.com>
2023-06-25 17:33:27 -0400

    did things

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://jomietv.xxsketchmcxx.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMjA3YjllZTBjMTQxNGQzMTI3YTMwMTMwOWViZTMyOSIsInN1YiI6IjY0OThhZWM5YjM0NDA5MDBmZmVjYmVjYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fUrzIh1d_aXoz0sAIcTKTvmcDOXm8_yVzTCWYuknU-Y"
+};
